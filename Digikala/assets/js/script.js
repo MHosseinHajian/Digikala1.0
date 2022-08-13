1fe0f2945644be32be6bd4366257c305eeadc332
Mohammad Hossein <92181912+MHosseinHajian@users.noreply.github.com>
2022-08-13 13:18:28 +0430

    Add Progress Bar Code

---
+++ Digikala/assets/js/script.js
@@ -0,0 +1,13 @@
+function main(){
+    function progressBar(){
+        console.log($(window).height())
+        let documentHeight = $(document).height() - $(window).height();
+        let docScrollTop = $(document).scrollTop()
+        let calculateBar= Math.floor((docScrollTop*100)/(documentHeight))
+        $('.progressBar2').css({
+            width: calculateBar+"%"
+        })
+    }
+    $(window).scroll(progressBar)
+}
+$(document).ready(main)
